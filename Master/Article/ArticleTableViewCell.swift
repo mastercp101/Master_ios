b9f1e7423ddb529a7ad538307f7d75d7b6cc6ab6
diego <rapid5218@hotmail.com>
2018-07-26 17:25:03 +0800

    Start Article View

---
+++ Master/Article/ArticleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ArticleTableViewCell.swift
+//  Master
+//
+//  Created by Diego on 2018/7/26.
+//  Copyright © 2018年 黎峻亦. All rights reserved.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

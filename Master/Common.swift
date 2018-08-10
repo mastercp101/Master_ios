55518439ebddecead8c7b0deaeb47dacf9d662a2
diego <rapid5218@hotmail.com>
2018-08-10 14:54:13 +0800

    0810 同步

--- Master/Common.swift
+++ Master/Common.swift
@@ -14,7 +14,7 @@ import UIKit
 // Connect DB URL
 let urlString = "http://127.0.0.1:8080/Master/"
 //哲維
-let urlString = "http://192.168.50.20:8080/Master/"
+//let urlString = "http://192.168.50.20:8080/Master/"
 //峻亦
 //let urlString = "http://192.168.50.245:8080/Master/"
 //let urlString = "http://172.20.10.8:8080/Master/"
